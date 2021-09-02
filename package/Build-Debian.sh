3b9bad2401cb1568687c1a129f4a83c6fc45b496
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2021-09-03 00:12:01 +0200

    Improve Linux bin package build

--- package/Build-Debian.sh
+++ package/Build-Debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Build uniblow Debian 10 binary package release
+# Build uniblow Debian/Ubuntu binary package release
 
 
 if ! (cat /etc/os-release | grep -E "10 \(buster\)" > /dev/null ) then
@@ -14,7 +14,9 @@ function is_installed {
     fi
 }
 
-softs_req=('libsdl2-2.0-0' 'python3-venv' 'python3-pip' 'python3-pyscard')
+
+
+softs_req=('libnotify4' 'libgtk-3-0' 'libpcsclite1' 'libsdl2-2.0-0' 'python3-venv' 'python3-pip' 'python3-pyscard')
 
 for softr in "${softs_req[@]}"; do
   if [ "$(is_installed $softr)" == 'not' ]; then
@@ -27,12 +29,14 @@ done
 
 rm -Rf dist
 
-echo Initializing venv ...
+echo Initializing the venv ...
 python3 -m venv --system-site-packages unibenvi
 source unibenvi/bin/activate
+python -m pip install -U pip
 
 echo Installing pip dependencies ...
-python -m pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10/wxPython-4.1.1-cp37-cp37m-linux_x86_64.whl wxPython < /dev/null
+
+python -m pip install https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10/wxPython-4.1.1-cp37-cp37m-linux_x86_64.whl < /dev/null
 python -m pip install -U pip setuptools-rust < /dev/null
 
 python setup.py install
