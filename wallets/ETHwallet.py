dac60b63f61773157b8637ab42ed7ef5c041a0b7
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2021-10-03 20:09:59 +0200

    Improve API calls reliability

--- wallets/ETHwallet.py
+++ wallets/ETHwallet.py
@@ -88,6 +88,7 @@ class ETHwalletCore:
         self.pubkey = decompress_pubkey(pubkey)
         key_hash = sha3(self.pubkey[1:])
         self.address = format_checksum_address(key_hash.hex()[-40:])
+        self.address_api = self.address.lower()
         self.ERC20 = ERC20
         self.api = api
         self.decimals = self.get_decimals()
@@ -95,12 +96,21 @@ class ETHwalletCore:
         self.chainID = chainID
 
     def getbalance(self, native=True):
+        if self.chainID in [1, 3, 4, 5, 42]:
+            # ETH and its testnets
+            block_state = "pending"
+        else:
+            # Polygon, BSC or Artbitrum
+            block_state = "latest"
         if native:
             # ETH native balance
-            return self.api.get_balance(f"0x{self.address}", "pending")
+            return self.api.get_balance(f"0x{self.address_api}", block_state)
         # ERC20 token balance
         balraw = self.api.call(
-            self.ERC20, BALANCEOF_FUNCTION, f"000000000000000000000000{self.address}", "pending"
+            self.ERC20,
+            BALANCEOF_FUNCTION,
+            f"000000000000000000000000{self.address_api}",
+            block_state,
         )
         if balraw == [] or balraw == "0x":
             return 0
@@ -123,7 +133,7 @@ class ETHwalletCore:
             return read_string(balraw)
 
     def getnonce(self):
-        numtx = self.api.get_tx_num(self.address, "pending")
+        numtx = self.api.get_tx_num(self.address_api, "pending")
         return numtx
 
     def prepare(self, toaddr, paymentvalue, gprice, glimit, data=bytearray(b"")):
