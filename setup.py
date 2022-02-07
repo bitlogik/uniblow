c9dcf9bbc692a64b9fd06f1937a30b23b32ae2c5
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2022-02-07 21:05:44 +0100

    Update pyweb3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "pysha3==1.0.2",
         "qrcode==6.1",
         "PyNaCl==1.5.0",
-        "pyweb3==0.1.4",
+        "pyweb3==0.1.5",
         "OpenPGPpy==0.8",
         "hidapi==0.11.0.post2",
         "pyWalletConnect==1.2.0",
