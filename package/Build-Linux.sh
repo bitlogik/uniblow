3428d2ed50bcdf3c2455cf93e6d3a3ca7387699d
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2022-04-28 17:18:56 +0200

    Back to Ubuntu 18 for build binary, for TailsOS

--- package/Build-Linux.sh
+++ package/Build-Linux.sh
@@ -3,8 +3,8 @@
 # Build uniblow Debian/Ubuntu binary package release
 
 
-if ! (cat /etc/os-release | grep -E "20\.04(\.[0-9]+)? LTS \(Focal Fossa\)" > /dev/null ) then
-  echo "This Uniblow building script only runs on Ubuntu 20.04."
+if ! (cat /etc/os-release | grep -E "18\.04(\.[0-9]+)? LTS \(Bionic Beaver\)" > /dev/null ) then
+  echo "This Uniblow building script only runs on Ubuntu 18.04."
   exit 1
 fi
 
@@ -34,7 +34,7 @@ python -m pip install -U pip
 
 echo Installing pip dependencies ...
 
-python -m pip install https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04/wxPython-4.1.1-cp38-cp38-linux_x86_64.whl
+python -m pip install https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-18.04/wxPython-4.1.1-cp36-cp36m-linux_x86_64.whl
 python -m pip install -U pip setuptools-rust
 
 python setup.py install
