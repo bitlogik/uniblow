5920416c20658bf304c918533abfaa51e3811976
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2022-11-09 13:56:51 +0100

    Clean comment

--- wallets/ETHtokens.py
+++ wallets/ETHtokens.py
@@ -118,7 +118,6 @@ nfts_values = [
     {
         "CryptoPunks": "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
         "BoredApeYachtClub": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-        # "ENS": "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85",
         "Zora": "0xabefbc9fd2f806065b4f3c237d4b59d9a97bcac7",
         "OpenSea Shared Storefront": "0x495f947276749ce646f68ac8c248420045cb7b5e",
         "Rarible": "0x60f80121c31a0d46b5279700f9df786054aa5ee5",
