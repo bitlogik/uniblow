c6526d3f975697cd32691b675e870ca71a6d29e0
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2023-01-25 20:04:27 +0100

    Change gas limit for ARB txs

--- wallets/ETHwallet.py
+++ wallets/ETHwallet.py
@@ -684,8 +684,14 @@ class ETH_wallet:
         # Transfer x unit to an account, pay
         if self.eth.contract:
             gazlimit = ETH_wallet.GAZ_LIMIT_ERC_20_TX
+            if self.chainID == 42161:
+                # Arbitrum : provide 4x more gas
+                gazlimit *= 4
         else:
             gazlimit = ETH_wallet.GAZ_LIMIT_SIMPLE_TX
+            if self.chainID == 42161:
+                # Arbitrum : provide 25x more gas
+                gazlimit *= 25
         if to_account.startswith("0x"):
             to_account = to_account[2:]
         gaz_price = self.eth.api.get_gasprice()  # wei per gaz unit
@@ -716,8 +722,14 @@ class ETH_wallet:
         # Transfer the amount in base unit minus fee, like the receiver paying the fee
         if self.eth.contract:
             gazlimit = ETH_wallet.GAZ_LIMIT_ERC_20_TX
+            if self.chainID == 42161:
+                # Arbitrum : provide 4x more gas
+                gazlimit *= 4
         else:
             gazlimit = ETH_wallet.GAZ_LIMIT_SIMPLE_TX
+            if self.chainID == 42161:
+                # Arbitrum : provide 25x more gas
+                gazlimit *= 25
         if to_account.startswith("0x"):
             to_account = to_account[2:]
         gaz_price = self.eth.api.get_gasprice()
