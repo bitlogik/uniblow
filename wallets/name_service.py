4dcb3fb65bc2a5889de81ce695d3ed8b89f9c717
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2023-03-09 14:16:08 +0100

    Fix klever domain resolution

--- wallets/name_service.py
+++ wallets/name_service.py
@@ -11,7 +11,7 @@ ZILresolver = "9611c53BE6d1b32058b2747bdeCECed7e1216793"
 
 
 ENS_TLDS = ["eth"]
-UD_TLDS = ["crypto", "nft", "blockchain", "bitcoin", "wallet", "888", "dao", "x", "lever", "hi"]
+UD_TLDS = ["crypto", "nft", "blockchain", "bitcoin", "wallet", "888", "dao", "x", "klever", "hi"]
 ZIL_TLDS = ["zil"]
 
 
