12a5fcdd135ef8ac8f5c51a9d2d76b7a4f1aa4c3
Toporin <satochip.wallet@gmail.com>
2023-07-12 11:09:50 +0100

    clean setup.py dependencies
    For windows build, the order of dependencies appears to affect pip installation.
    There is a (minor) conflict for pyscard dependency, as pysatochip requires v>=1.9.9 (resolving to latest 2.0.7), while openpgp requires exactly 2.0.3.
    If v2.0.7 is installed before 2.03, it generates an error and some unrelated dependencies (h11, cffi,...) are not installed for an unknown reason.
    By simply moving pysatochip up in 'install_requires' list, pyscard v2.0.3 is installed and conflict is avoided...

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         "pysatochip==0.12.5",
         "wxPython>=4.0.7",
-        "cryptography==41.0.1", # >=3.3
+        "cryptography>=3.3",
         "pysha3==1.0.2",
         "qrcode==6.1",
         "PyNaCl==1.5.0",
@@ -35,7 +35,6 @@ setup(
         "OpenPGPpy==1.1",
         "hidapi==0.11.0.post2",
         "pyWalletConnect==1.4.0",
-        #"cffi>=1.4.1",
     ],
     include_package_data=False,
     classifiers=[
