13d6b6336df33466de7378c84c9e7effa05dda97
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2024-06-02 16:35:03 -0300

    Update OpenPGPpy
    Improve OpenPGP hardware device

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "qrcode==6.1",
         "PyNaCl==1.5.0",
         "pyweb3==0.1.7",
-        "OpenPGPpy==1.1",
+        "OpenPGPpy==1.2",
         "hidapi==0.14.0",
         "pyWalletConnect==1.6.2",
     ],
