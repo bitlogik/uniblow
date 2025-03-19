c5ad706d604084bf3e1e775e8860eedf406b1af0
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2025-03-18 21:10:43 -0300

    Try fix build mac in CICD

--- package/Build-Mac.sh
+++ package/Build-Mac.sh
@@ -17,6 +17,7 @@ source unibenv/bin/activate
 echo Installing pip dependencies ...
 python -m pip install -U pip
 python -m pip install -U swig
+python -m pip install Pillow
 python -m pip install OpenPGPpy --no-deps
 python -m pip install pyscard==2.0.10
 python -m pip install wxPython>=4.2.0
